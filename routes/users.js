9905657ee01e4cfe39ea57a5a018f577c309141a
yisichoco <yisichoco@gmail.com>
2022-05-05 11:13:18 +0800

    Ajax FormData 檔案上傳

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,26 @@ const multer = require('multer');
 var router = express.Router();
 
 //沒有上傳檔案的multe設定
-const upload = multer();
+//const upload = multer();
+
+//上傳檔案的multer設定
+const ext = {
+  'image/jpeg':'.jpg',
+  'image/png':'.png',
+  'image/gif':'.gif',
+}
+const storage = multer.diskStorage({
+  destination:(req,file,cb)=>{
+    cb(null, 'public/uploads');
+  },
+  filename:(req,file,cb)=>{
+     cb(null, new Date().getTime() + ext[file.mimetype]);
+  }
+})
+const fileFilter = (req,file,cb)=>{
+  cb(null, !!ext[file.mimetype]);
+}
+const upload = multer({storage, fileFilter});
 
 /* GET users listing. */
 //http://localhost:3000/users?name=Jack&age=20
@@ -30,9 +49,22 @@ router.get('/register',(req,res)=>{
 
 //POST http://localhost:3000/users/register
 //沒有上傳檔案的FormData使用 upload.none() middleware
-router.post('/register',upload.none(),(req,res)=>{
-  //req.body 接收表單透過POST fromdata 傳過來的資料
-  res.send(`POST:${req.body.email} - ${req.body.pwd}`)
+// router.post('/register',upload.none(),(req,res)=>{
+//   //req.body 接收表單透過POST fromdata 傳過來的資料
+//   res.send(`POST:${req.body.email} - ${req.body.pwd}`)
+// })
+
+
+//POST http://localhost:3000/users/register
+//沒有上傳檔案的FormData使用 upload.none() middleware
+router.post('/register',upload.single("photo"),(req,res)=>{
+  //req.body 接收表單透過POST fromdata 傳過來的文字資料
+  //req.file 會接收上傳的檔案
+  console.log(req.body);
+  console.log(req.file)
+  //res.send(`POST:${req.body.email} - ${req.body.pwd}`)
+  res.send(`上傳檔案名稱為 ${req.file.filename}`)
 })
+
 
 module.exports = router;
