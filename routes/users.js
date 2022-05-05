59f8b450ff1219201cc1ed44e6297438a61ff857
yisichoco <yisichoco@gmail.com>
2022-05-05 10:17:27 +0800

    multipart form-data 的資料傳送及接收

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,9 @@
 var express = require('express');
+const multer = require('multer');
 var router = express.Router();
+
+//沒有上傳檔案的multe設定
+const upload = multer();
 
 /* GET users listing. */
 //http://localhost:3000/users?name=Jack&age=20
@@ -17,9 +21,17 @@ router.get('/register',(req,res)=>{
   res.render('register',{ title: 'Ajax POST Demo' })
 })
 
+// //POST http://localhost:3000/users/register
+// router.post('/register',(req,res)=>{
+//   //req.body 接收表單透過POST form-urlencoded 傳過來的資料
+//   res.send(`POST:${req.body.email} - ${req.body.pwd}`)
+// })
+
+
 //POST http://localhost:3000/users/register
-router.post('/register',(req,res)=>{
-  //req.body 接收表單透過POST form-urlencoded 傳過來的資料
+//沒有上傳檔案的FormData使用 upload.none() middleware
+router.post('/register',upload.none(),(req,res)=>{
+  //req.body 接收表單透過POST fromdata 傳過來的資料
   res.send(`POST:${req.body.email} - ${req.body.pwd}`)
 })
 
