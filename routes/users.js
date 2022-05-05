e77f7aab24bb6f360597f27f7e96080e49752010
yisichoco <yisichoco@gmail.com>
2022-05-05 09:56:06 +0800

    透過POST傳送資料，伺服器接收

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-//http://localhost:3000/users/
+//http://localhost:3000/users?name=Jack&age=20
 router.get('/', function(req, res, next) {
   //接收Ajax透過GET傳過來的資料
   const name = req.query.name ? req.query.name : "Ajax";
@@ -12,12 +12,15 @@ router.get('/', function(req, res, next) {
   
 });
 
-//http://localhost:3000/users/register
+//GET http://localhost:3000/users/register
 router.get('/register',(req,res)=>{
   res.render('register',{ title: 'Ajax POST Demo' })
 })
 
-
-
+//POST http://localhost:3000/users/register
+router.post('/register',(req,res)=>{
+  //req.body 接收表單透過POST form-urlencoded 傳過來的資料
+  res.send(`POST:${req.body.email} - ${req.body.pwd}`)
+})
 
 module.exports = router;
