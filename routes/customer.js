7ae29b0f85e4b88433ddf79ac4546a724ab14791
yisichoco <yisichoco@gmail.com>
2022-05-05 14:25:03 +0800

    根據國家將城市明載入到Select標籤中

--- routes/customer.js
+++ routes/customer.js
@@ -10,6 +10,23 @@ router.get('/country',(req,res,next)=>{
        res.json(results);
     })
 })
+//http://localhost:3000/customers/city?country=USA
+router.get('/city',(req,res,next)=>{
+    const sql = "select distinct city from customers where country=? order by city";   
+    connection.query(sql,[req.query.country],function(err,results,fields){
+        console.log(err)
+       res.json(results);
+    })
+})
+
+//http://localhost:3000/customers/customers?city=Portland
+router.get('/customers',(req,res,next)=>{
+    const sql = "select * from northwind.customers where city=?";   
+    connection.query(sql,[req.query.city],function(err,results,fields){
+        console.log(err)
+       res.json(results);
+    })
+})
 
 
 module.exports = router;
